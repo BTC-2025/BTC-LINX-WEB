b927356e490290521fb9b1e95d9910bd6511625f
BTC001B <brsridharan15@gmail.com>
2025-11-14 14:33:46 +0530

    wrap

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -1,8 +1,5 @@
-"use client";
-
 import type { Metadata } from 'next';
-import Header from '@/components/common/Header';
-import HomepageInteractive from './components/HomepageInteractive';
+import HomepageWrapper from './HomepageWrapper';
 
 export const metadata: Metadata = {
   title: 'Homepage - BTC Linx Enterprise',
@@ -10,12 +7,5 @@ export const metadata: Metadata = {
 };
 
 export default function Homepage() {
-  return (
-    <main className="min-h-screen bg-background">
-      <Header />
-      <div className="pt-16">
-        <HomepageInteractive />
-      </div>
-    </main>
-  );
+  return <HomepageWrapper />;
 }
